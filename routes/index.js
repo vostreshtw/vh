178fc1a8a6ece948df13e95dba9d6bc473c172a9
Eugenia Vostres <s0566769@htw-berlin.de>
2019-09-26 18:15:25 +0200

    First commit

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('index', {
+    name: ' '
+}));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+    res.render('dashboard', {
+        name: req.user.name
+    })
+);
+
+// Crud
+router.get('/crud', ensureAuthenticated, (req, res) =>
+    res.render('crud', {
+        name: req.user.name
+    })
+);
+
+
+// Angebot
+router.get('/angebot', (req, res) =>
+    res.render('angebot', {
+        name: ' '
+    })
+);
+
+// Impressum
+router.get('/impressum', (req, res) =>
+    res.render('impressum', {
+        name: ' '
+    })
+);
+
+// Search
+router.get('/search', (req, res) =>
+    res.render('search', {
+        name: ' '
+    })
+);
+
+module.exports = router;
